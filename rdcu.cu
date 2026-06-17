03eb5353e6234378d74950b4dac6f8e743087b59
Zachary Nicolaou <znicolao@erph1709.cn.isaac.utk.edu>
2026-06-17 18:09:56 -0400

    validated spatial derivatives

--- rdcu.cu
+++ rdcu.cu
@@ -1,7 +1,5 @@
-//Zachary G. Nicolaou 2/10/2024
-//Integrate the Kuramoto model with adaptive Runke Kutta timestepping on a gpu
-//Default adjacency, initial conditions, and frequencies follow volcano
-//nvcc -lcuda -lcublas -lcurand -O3 -o kuramoto_64 dp45_64.cu kuramoto_64.cu
+//Zachary G. Nicolaou 6/16/2026
+//nvcc -lcufft -lcublas -O3 -o rdcu dp45_64.cu rdcu.cu
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -15,10 +13,13 @@ typedef struct parameters
 {
   cublasHandle_t handle;
   cufftHandle *plans;
-  unsigned long int N;
-  double *Y;
+  int N;
+  int n;
+  int ndim;
+  int *Ns;
+  cufftDoubleComplex *Y;
   cufftDoubleComplex *yfft;
-  double *theta;
+  int *index;
   int *eta;
   int *nu;
   int N_eta;
@@ -36,7 +37,7 @@ typedef struct parameters
 }parameters;
 
 
-__global__ void make_dict (double* Y, const unsigned long int N, double* dict, const unsigned int N_eta, int* eta, int* nu) {
+__global__ void make_dict (double* Y, const int N, double* dict, const int N_eta, int* eta, int* nu) {
   int i = blockIdx.x*blockDim.x + threadIdx.x;
   if (i<N){
     for (int j=0; j<N_eta; j++){
@@ -45,18 +46,50 @@ __global__ void make_dict (double* Y, const unsigned long int N, double* dict, c
   }
 }
 
+__global__ void d1 (cufftDoubleComplex* Yin, cufftDoubleComplex* Yout, const int N, const int n, int *Ns, const int ndim, const int axis) {
+  int i = blockIdx.x*blockDim.x + threadIdx.x;
+  if (i<N*n){
+    //find the index for the specified axis
+    int j=i%N;
+    for (int d=ndim-1; d>axis; d--){
+      j/=Ns[d];
+    }
+    int na=j%Ns[axis];
+
+    //multiply by frequency in the specified axis
+    double freq=na*1.0/Ns[axis];
+    if (freq>0.5){
+      freq=freq-1.0;
+    }
+    Yout[i].x=-freq*Yin[i].y;
+    Yout[i].y=freq*Yin[i].x;
+  }
+}
+
 
 void makeY (double *y, void *pars){
   parameters *p = (parameters *)pars;
-  cublasDcopy(p->handle, p->n*p->N, y, 1, (double *)(p->yfft), 2);
-  cufftExecZ2Z(p->plans[0], p->yfft, p->yfft, CUFFT_FORWARD);
-  //multiply by frequencies and take inverse fft
+  cublasDcopy(p->handle, p->N*p->n, y, 1, (double *)(p->Y), 2);
+  // cudaMemcpy(p->Y,y, p->N*p->n*sizeof(double), cudaMemcpyDeviceToDevice);
+
+  cufftExecZ2Z(p->plans[0], p->Y, p->yfft, CUFFT_FORWARD);
+  for (int i=0; i<p->ndim; i++){
+    d1<<<(p->N*p->n+255)/256, 256>>>(p->yfft, &(p->yfft[(i+1)*p->N*p->n]), p->N, p->n, p->Ns, p->ndim, i);
+    for (int j=0; j<p->ndim; j++){
+      d1<<<(p->N*p->n+255)/256, 256>>>(&(p->yfft[(i+1)*p->N*p->n]), &(p->yfft[(j+i*p->ndim+p->ndim+1)*p->N*p->n]), p->N, p->n, p->Ns, p->ndim, j);
+    }
+  }
+  cufftExecZ2Z(p->plans[1], p->yfft, p->Y, CUFFT_INVERSE);
+  double scale=1.0/p->N;
+  cublasDscal(p->handle, 2*p->N*p->n*(1+p->ndim+p->ndim*p->ndim), &scale, (double *)p->Y, 1);
+  //Add the length scales for first and second derivatives too
 }
 void dydt (double t, double *y, double *f, void *pars){
   parameters *p = (parameters *)pars;
 
   makeY(y, pars);
-  make_dict<<<(p->N+255)/256, 256>>>(p->Y, p->N, p->theta, p->N_eta, p->eta, p->nu);
+  //loop through the dictionary terms and add monomials to f
+  // make_dict<<<(p->N+255)/256, 256>>>(p->Y, p->N, p->theta, p->N_eta, p->eta, p->nu);
 
 }
 
@@ -91,7 +124,7 @@ void step_eval(double t, double h, double* y, void *pars){
   FILE *outanimation;
   if(p->dense>=1){
     strcpy(file,p->filebase);
-    strcat(file, "thetas.dat");
+    strcat(file, "eval.dat");
     outanimation = fopen(file,"ab");
   }
 
@@ -99,7 +132,8 @@ void step_eval(double t, double h, double* y, void *pars){
     double *y_eval;
     y_eval=dp45_eval(t,p->t_eval[p->eval_i]);
     if(p->dense>=1){
-      cublasGetVector(p->N, sizeof(double), y_eval, 1, p->yloc, 1);
+      // cublasGetVector(p->N, sizeof(double), y_eval, 1, p->yloc, 1);
+      cudaMemcpy(p->yloc, y_eval, p->N*sizeof(double), cudaMemcpyDeviceToHost);
       fwrite(p->yloc,sizeof(double),p->N,outanimation);
       fflush(outanimation);
     }
@@ -110,7 +144,8 @@ void step_eval(double t, double h, double* y, void *pars){
     fclose(outanimation);
   }
 
-  cublasGetVector(p->N, sizeof(double), y, 1, p->yloc, 1);
+  // cublasGetVector(p->N, sizeof(double), y, 1, p->yloc, 1);
+  cudaMemcpy(p->yloc, y, p->N*sizeof(double), cudaMemcpyDeviceToHost);
 
   strcpy(file,p->filebase);
   strcat(file,"fs.dat");
@@ -127,86 +162,108 @@ int main (int argc, char* argv[]) {
     struct timeval start,end;
     gettimeofday(&start,NULL);
 
-    double t1, dt;
-    double atl, rtl, I;
-    int gpu, seed, fixed;
-    unsigned long int N, n;
+    double t1=1e2, dt=1e0;
+    double atl=1e-6, rtl=0, I=1.0;
+    int gpu=0, seed=1, fixed=0;
+    int n=1, ndim=1;
     char* filebase;
-
-    N=128;
-    n=1;
-    I=1;
-
-    t1=1e2;
-    dt=1e0;
-    gpu=0;
-    seed=1;
     int verbose=0;
-    rtl=0;
-    atl=1e-6;
-    fixed=0;
     char c;
     int help=1;
     int dense=3;
     int reload=0;
-    int Ns[100];
-    int ndim=0;    const char delim[] = ",";
-    char* token;
-
-
+    int Nsloc[3]={128,128,128};
+    const char delim[] = ",";
+  
     while (optind < argc) {
       if ((c = getopt(argc, argv, "N:n:I:D:g:t:d:s:r:a:hvFR")) != -1) {
         switch (c) {
-          case 'N':
-              token = strtok(optarg, delim);
+          case 'N': {
+            ndim=0;
+            if (optarg != NULL) {
+              char *optarg_copy = strdup(optarg);
+              char *token = strtok(optarg_copy, delim);
               while (token != NULL) {
-                Ns[ndim++]=(int)atoi(token);
+                Nsloc[ndim++]=(int)atoi(token);
                 token = strtok(NULL, delim);
                 if (ndim>3){
                   printf("Too many dimension!");
                   return 0;
                 }
               }
-              break;
-          case 'n':
+            }
+            break;
+          }
+          case 'n': {
+            if (optarg != NULL) {
               n = (int)atoi(optarg);
-              break;
-          case 'I':
+            }
+            break;
+          }
+          case 'I': {
+            if (optarg != NULL) {
               I = (double)atof(optarg);
-              break;
-          case 'g':
-              gpu = (double)atof(optarg);
-              break;
-          case 't':
-              t1 = (double)atof(optarg);
-              break;
-          case 'd':
-              dt = (double)atof(optarg);
-              break;
-          case 's':
-              seed = (int)atoi(optarg);
-              break;
-          case 'r':
-              rtl = (double)atof(optarg);
-              break;
-          case 'a':
-              atl = (double)atof(optarg);
-              break;
-          case 'D':
+            }
+            break;
+          }
+          case 'D':{
+            if (optarg != NULL) {
               dense = (int)atoi(optarg);
-              break;
-          case 'R':
-              reload = 1;
-              break;
-          case 'F':
-              fixed = 1;
-              break;
-          case 'h':
-              help=1;
-              break;
-          case 'v':
-              verbose=1;
-              break;
+            }
+            break;
+          }
+          case 'g': {
+            if (optarg != NULL) {
+              gpu = (int)atoi(optarg);
+            }
+            break;
+          }
+          case 't': {
+            if (optarg != NULL) {
+              t1 = (double)atof(optarg);
+            }
+            break;
+          }
+          case 'd': {
+            if (optarg != NULL) {
+              dt = (double)atof(optarg);
+            }
+            break;
+          }
+          case 's': {
+            if (optarg != NULL) {
+              seed = (int)atoi(optarg);
+            }
+            break;
+          }
+          case 'r': {
+            if (optarg != NULL) {
+              rtl = (double)atof(optarg);
+            }
+            break;
+          }
+          case 'a': {
+            if (optarg != NULL) {
+              atl = (double)atof(optarg);
+            }
+            break;
+          }
+          case 'R': {
+            reload = 1;
+            break;
+          }
+          case 'F': {
+            fixed = 1;
+            break;
+          }
+          case 'h': {
+            help=1;
+            break;
+          }
+          case 'v': {
+            verbose=1;
+            break;
+          }
         }
       }
       else {
@@ -242,8 +299,8 @@ int main (int argc, char* argv[]) {
       exit(0);
     }
 
-    double t=0,h;
-    int i=0,j=0;
+    double t=0,h=1;
+    int i=0,j=0,k=0;
     FILE *out, *in;
 
     char file[256];
@@ -251,18 +308,20 @@ int main (int argc, char* argv[]) {
     strcat(file,".out");
     out = fopen(file,"ab");
 
-    double *yloc, *y, *Y, *theta;
-    cufftDoubleComplex *yfft, *yfftloc;
-    N=Ns[0];
+    int *Ns;
+    double *yloc, *y;
+    cufftDoubleComplex *Yloc, *Y, *yfft, *yfftloc;
+    int N=Nsloc[0];
     for (i=1; i<ndim; i++){
-      N*=Ns[i];
+      N*=Nsloc[i];
     }
-    yloc = (double*)calloc(n*N,sizeof(double));
-    yfftloc = (cufftDoubleComplex*)calloc(n*N,sizeof(cufftDoubleComplex));
+    yloc = (double*)calloc(N*n,sizeof(double));
+    Yloc = (cufftDoubleComplex*)calloc(N*n*(1+ndim+ndim*ndim),sizeof(cufftDoubleComplex));
+    yfftloc = (cufftDoubleComplex*)calloc(N*n*(1+ndim+ndim*ndim),sizeof(cufftDoubleComplex));
 
     cublasStatus_t stat;
     cublasHandle_t handle;
-    cufftHandle *plans = (cufftHandle*) malloc(1 * sizeof(cufftHandle));
+    cufftHandle *plans = (cufftHandle*) malloc(2 * sizeof(cufftHandle));
 
     cudaSetDevice(gpu);
     stat = cublasCreate(&handle);
@@ -278,26 +337,10 @@ int main (int argc, char* argv[]) {
     }
     fprintf(out, "\n");
 
-
-//     size_t fr, total, req;
-//     cudaMemGetInfo (&fr, &total);
-//     req=(100+N)*N*sizeof(double);
-//
-//     printf("GPU Memory: %lu %lu %lu\n", fr, total, req);
-//     fprintf(out,"GPU Memory: %lu %lu %lu\n", fr, total, req);
-//     if(fr < req) {
-//       printf("GPU Memory low!\n");
-//       fprintf(out,"GPU Memory low!\n");
-//       return 0;
-//     }
-//     fflush(out);
-
-    int num_derivs = 1+ndim+(ndim*(ndim+1))/2;
-    cudaMalloc ((void**)&y, n*N*sizeof(double));
-    cudaMalloc ((void**)&yfft, n*N*sizeof(cufftDoubleComplex));
-    cudaMalloc ((void**)&Y, num_derivs*n*N*sizeof(double));
-    cudaMalloc ((void**)&theta, n*N*sizeof(double));
-
+    cudaMalloc ((void**)&Ns, ndim*sizeof(int));
+    cudaMalloc ((void**)&y, N*n*sizeof(double));
+    cudaMalloc ((void**)&yfft, N*n*(1+ndim+ndim*ndim)*sizeof(cufftDoubleComplex));
+    cudaMalloc ((void**)&Y, N*n*(1+ndim+ndim*ndim)*sizeof(cufftDoubleComplex));
     if (fixed){
       h = dt;
     }
@@ -339,15 +382,19 @@ int main (int argc, char* argv[]) {
       for(i=0; i<n; i++){
         for(j=0; j<N; j++) {
           yloc[N*i+j] = I/2*(2.0/RAND_MAX*rand()-1);
-          yfftloc[N*i+j] = {0.0,0.0};
+          for(k=0; k<(1+ndim+ndim*ndim); k++) {
+            yfftloc[N*i*(1+ndim+ndim*ndim)+j*(1+ndim+ndim*ndim)+k] = {0.0,0.0};
+          }
         }
       }
+
       in=fopen(file,"wb");
-      fwrite(yloc,sizeof(double),n*N,in);
+      fwrite(yloc,sizeof(double),N*n,in);
+      fwrite(&t,sizeof(double),1,in);
+      fwrite(&h,sizeof(double),1,in);
       fclose(in);
+
     }
-
-
 
     int n0=int(t/dt)+1;
     int n1=int(t1/dt)+1;
@@ -359,24 +406,34 @@ int main (int argc, char* argv[]) {
     }
 
     
-    cufftPlanMany(plans, ndim, Ns, NULL, 1, 0, NULL, 1, 0, CUFFT_Z2Z, n);
-    parameters pars={.handle=handle, .plans=plans, .N=N, .Y=Y, .yfft=yfft, .theta=theta, .t0=t, .t1=t1, .steps=0, .verbose=verbose, .dense=dense, .t_eval=t_eval, .n_eval=n_eval, .eval_i=0, .yloc=yloc, .filebase=filebase,.start=start};
+    // cufftPlanMany(&(plans[0]), ndim, Nsloc, Nsloc, 1, N, Nsloc, 1, N, CUFFT_D2Z, n);
+    // cufftPlanMany(&(plans[1]), ndim, Nsloc, Nsloc, 1, N, Nsloc, 1, N, CUFFT_Z2D, n*(1+ndim+ndim*ndim));
+    cufftPlanMany(&(plans[0]), ndim, Nsloc, Nsloc, 1, N, Nsloc, 1, N, CUFFT_Z2Z, n);
+    cufftPlanMany(&(plans[1]), ndim, Nsloc, Nsloc, 1, N, Nsloc, 1, N, CUFFT_Z2Z, n*(1+ndim+ndim*ndim));
+    parameters pars={.handle=handle, .plans=plans, .N=N, .n=n, .ndim=ndim, .Ns=Ns, .Y=Y, .yfft=yfft, .t0=t, .t1=t1, .steps=0, .verbose=verbose, .dense=dense, .t_eval=t_eval, .n_eval=n_eval, .eval_i=0, .yloc=yloc, .filebase=filebase,.start=start};
 
     y=dp45_init(n*N, atl, rtl, fixed, yloc, handle, &dydt);
-    cublasSetVector(n*N, sizeof(double), yloc, 1, y, 1);
-    cublasSetVector(n*N, sizeof(cufftDoubleComplex), yfftloc, 1, yfft, 1);
+    cudaMemcpy(yfft, yfftloc, N*n*(1+ndim+ndim*ndim)*sizeof(cufftDoubleComplex), cudaMemcpyHostToDevice); 
+    cudaMemcpy(Ns, Nsloc, ndim*sizeof(int), cudaMemcpyHostToDevice);
 
     //////////////////////////////////////////////////////////////////////////
-
-
-
-    cublasGetVector(n*N, sizeof(double), p->Y, 1, yloc, 1);
-
+    makeY(y, &pars);
+    cudaMemcpy(yloc, y, N*n*sizeof(double), cudaMemcpyDeviceToHost);
     strcpy(file,filebase);
-    strcat(file,"Y.dat");
+    strcat(file,"y.dat");
     FILE *outtmp=fopen(file,"wb");
 
-    fwrite(yloc,sizeof(double),n*N,outtmp);
+    fwrite(yloc,sizeof(double),N*n,outtmp);
+
+    fflush(outtmp);
+    fclose(outtmp);
+
+    cudaMemcpy(Yloc, Y, N*n*(1+ndim+ndim*ndim)*sizeof(cufftDoubleComplex), cudaMemcpyDeviceToHost);
+    strcpy(file,filebase);
+    strcat(file,"Y.dat");
+    outtmp=fopen(file,"wb");
+
+    fwrite(Yloc,sizeof(cufftDoubleComplex),N*n*(1+ndim+ndim*ndim),outtmp);
 
     fflush(outtmp);
     fclose(outtmp);
@@ -405,7 +462,8 @@ int main (int argc, char* argv[]) {
     //final state output with coupling appended
     parameters *p = &pars;
     y_eval=dp45_eval(t,p->t_eval[p->n_eval-1]);
-    cublasGetVector(p->N, sizeof(double), y_eval, 1, p->yloc, 1);
+    // cublasGetVector(p->N, sizeof(double), y_eval, 1, p->yloc, 1);
+    cudaMemcpy(p->yloc, y_eval, p->N*sizeof(double), cudaMemcpyDeviceToHost);
 
     strcpy(file,p->filebase);
     strcat(file,"fs.dat");
@@ -429,10 +487,15 @@ int main (int argc, char* argv[]) {
     fclose(out);
 
     free(yloc);
+    free(Yloc);
+    free(yfftloc);
+    free(Nsloc);
     cudaFree(y);
     cudaFree(yfft);
+    // cudaFree(yfft2);
+    // cudaFree(yfft3);
     cudaFree(Y);
-    cudaFree(theta);
+    cudaFree(Ns);
 
     dp45_destroy();
 
