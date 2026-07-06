447febe830975120964fb41065b5786d7b540dcc
Zachary Nicolaou <znicolao@erph1709.cn.isaac.utk.edu>
2026-07-06 04:23:43 -0400

    cleaned up bdf

--- bdf.cu
+++ bdf.cu
@@ -6,17 +6,16 @@
 #include "bdf.h"
 
 //bdf coefficients
-const double kappaloc[6] = {0.0000000000000000,-0.1850000000000000,-0.1111111111111111,-0.0823000000000000,-0.0415000000000000,0.0000000000000000};
-const double gammabdfloc[6] = {0.0000000000000000,1.0000000000000000,1.5000000000000000,1.8333333333333333,2.0833333333333330,2.2833333333333332};
-const double alphaloc[6] = {0.0000000000000000,1.1850000000000001,1.6666666666666667,1.9842166666666667,2.1697916666666663,2.2833333333333332};
-const double betaloc[6] = {1.0000000000000000,0.3150000000000000,0.1666666666666667,0.0991166666666667,0.1135416666666667,0.1666666666666667};
+const double kappaloc[6] = {0.0,-0.1850,-1.0/9,-0.0823,-0.0415,0.0};
+const double gammabdfloc[6] = {0.0,1.0,1.0+1.0/2,1.0+1.0/2+1.0/3,1.0+1.0/2+1.0/3+1.0/4,1.0+1.0/2+1.0/3+1.0/4+1.0/5};
 
-static double *kappa, *gammabdf, *alpha, *beta;
+static double *gammabdf;
 static double one=1.0, nconststeps=0;
 static double *y, *yscale, *f, *d, *dd, *b, *D, *Dnew, *ytemp, *yerr, *y_eval, *vals, *Ivals, *Avals, ntol;
 static double R[36], U[36], RUloc[36];
 static double *RU;
-static int *rows, *cols, *Irows, *Icols, *Arows, *Acols;
+static int *rows, *cols, *Irows, *Icols, *Arows, *Acols, nstep=0;
+static void *buffer;
 
 static unsigned long int N;
 static void (*dydt)(double, double*, double*, void*) = NULL;
@@ -86,7 +85,7 @@ __global__ void updateD (double *D, double *Dnew, const unsigned long int N, int
 __global__ void errscale (double *y, double atl, double rtl, const unsigned long int N){
   int i = blockIdx.x*blockDim.x + threadIdx.x;
   if(i<N){
-    y[i]=rtl*abs(y[i])+atl;
+    y[i]=atl+rtl*fabs(y[i]);
   }
 }
 
@@ -102,14 +101,13 @@ __global__ void step0 (double *y, double *D, const unsigned long int N, int orde
 }
 
 // RHS for Newton solver step
-__global__ void rhs (double *b, double *f, double *d, double *D, const unsigned long int N, int order, double h, double *gammabdf, double *alpha){
+__global__ void rhs (double *b, double *f, double *d, double *D, const unsigned long int N, int order, double h, double *gammabdf, double alpha){
   int i = blockIdx.x*blockDim.x + threadIdx.x;
   if(i<N){
-    b[i]=h*f[i];
+    b[i]=h/alpha*f[i];
     for (int j=1; j<=order; j++){
-      b[i]-=D[N*j+i]*gammabdf[j];
+      b[i]-=D[N*j+i]*gammabdf[j]/alpha;
     }
-    b[i]/=alpha[order];
     b[i]-=d[i];
   }
 }
@@ -133,14 +131,8 @@ __global__ void scale_err (double *d, double *yscale, double *yerr, const unsign
   }
 }
 
-void makeA(int *nnzA, double factor){
-  size_t bufferSize = 0;
-  void* buffer;
-  cusparseDcsrgeam2_bufferSizeExt(sphandle,N,N,&one,Idesc,N,Ivals,Irows,Icols,&factor,Jdesc,nnz,vals,rows,cols,Adesc,Avals,Arows,Acols,&bufferSize);
-  cudaMalloc(&buffer, bufferSize);
-  cusparseXcsrgeam2Nnz(sphandle,N,N,Idesc,N,Irows,Icols,Jdesc,nnz,rows,cols,Adesc,Arows,nnzA,buffer);
+void makeA(double factor){
   cusparseDcsrgeam2(sphandle,N,N,&one,Idesc,N,Ivals,Irows,Icols,&factor,Jdesc,nnz,vals,rows,cols,Adesc,Avals,Arows,Acols,buffer); 
-  cudaFree(buffer);
 }
 
 
@@ -150,10 +142,13 @@ int bdf_step (double *t, double *h, void* pars){
   double dnorm=0, dnormold=0,rate=0,zero=0.0,one=1.0,mone=-1.0,factor=1.0;
   //Calculate the predictor and store in ytemp
   step0<<<(N+255)/256, 256>>>(ytemp, D, N, order);
+  //find the scale for the newton iteration stopping 
   cudaMemcpy(yscale, ytemp, N*sizeof(double), cudaMemcpyDeviceToDevice);
   errscale<<<(N+255)/256, 256>>>(yscale, atl, rtl, N);
+  // cublasDnrm2(handle, N, yscale, 1, &dnorm);
+  // dnorm /= pow(N,0.5);
+  // printf("yscale %e\n", dnorm);
 
-  //find the scale for the newton iteration stopping 
   cublasDscal(handle, N, &zero, d, 1);
   
   //Newton solver for implicit equation
@@ -162,18 +157,19 @@ int bdf_step (double *t, double *h, void* pars){
     (*dydt)((*t)+(*h),ytemp,f,pars);
     //refactor to find LU if needed
     if(refactor){
-      makeA(&nnzA, -(*h)/(alphaloc[order]));
+      makeA(-(*h)/((1-kappaloc[order])*gammabdfloc[order]));
       cudssExecute(dsshandle, CUDSS_PHASE_REFACTORIZATION, config, data, A, dddss, bdss);
       refactor=0;
     }
     //calculate rhs and solve to find dd
-    rhs<<<(N+255)/256, 256>>>(b, f, d, D, N, order, *h, gammabdf, alpha);
+    rhs<<<(N+255)/256, 256>>>(b, f, d, D, N, order, *h, gammabdf, (1-kappaloc[order])*gammabdfloc[order]);
     cudssExecute(dsshandle, CUDSS_PHASE_SOLVE, config, data, A, dddss, bdss);
 
     //scale d and calculate norm for convergence test
     scale_err<<<(N+255)/256, 256>>>(dd, yscale, yerr, N);
     cublasDnrm2(handle, N, yerr, 1, &dnorm);
     dnorm /= pow(N,0.5);
+    // printf("ddnorm %i %e\n", it, dnorm);
     if (dnormold>0){
       rate=dnorm/dnormold;
     }
@@ -218,8 +214,7 @@ int bdf_step (double *t, double *h, void* pars){
       updateD<<<(N+255)/256, 256>>>(D, Dnew, N, order, RU);
       cudaMemcpy(D, Dnew, (order+1)*N*sizeof(double), cudaMemcpyDeviceToDevice);
 
-      refactor=1;
-      rejac=1;
+      refactor=1; //optional?
       nconststeps=0;
       return 0;
     }
@@ -237,10 +232,13 @@ int bdf_step (double *t, double *h, void* pars){
 
   cudaMemcpy(yscale, ytemp, N*sizeof(double), cudaMemcpyDeviceToDevice);
   errscale<<<(N+255)/256, 256>>>(yscale, atl, rtl, N);
+  // cublasDnrm2(handle, N, yscale, 1, &dnorm);
+  // dnorm /= pow(N,0.5);
+  // printf("yscale2 %e\n", dnorm);
   cudaMemcpy(yerr, d, N*sizeof(double), cudaMemcpyDeviceToDevice);
-  scale_err<<<(N+255)/256, 256>>>(yerr, yscale, yerr, N);
+  scale_err<<<(N+255)/256, 256>>>(d, yscale, yerr, N);
   cublasDnrm2(handle, N, yerr, 1, &errnorm);
-  errnorm *= betaloc[order]/pow(N,0.5);
+  errnorm *= (kappaloc[order]*gammabdfloc[order]+1.0/(order+1))/pow(N,0.5);
   factor=safety * pow(errnorm, (-1.0 / (order + 1)));
 
   //reject and decrease h
@@ -254,13 +252,14 @@ int bdf_step (double *t, double *h, void* pars){
       cudaMemcpy(RU, RUloc, (order+1)*(order+1)*sizeof(double), cudaMemcpyHostToDevice);
       updateD<<<(N+255)/256, 256>>>(D, Dnew, N, order, RU);
       cudaMemcpy(D, Dnew, (order+1)*N*sizeof(double), cudaMemcpyDeviceToDevice);
-      //refactor=1;
+      refactor=1; //optional?
 
       nconststeps=0;
       return 0;
   }
   
   //accept; update t, y, and D
+  // printf("step %i %i %e\n", ++nstep, order, (*h));
   (*t)=(*t)+(*h);
   cudaMemcpy(y, ytemp,  N*sizeof(double), cudaMemcpyDeviceToDevice);
   rejac=1;
@@ -278,21 +277,23 @@ int bdf_step (double *t, double *h, void* pars){
     double errnormminus=INFINITY, errnormplus=INFINITY, factorminus=0.0, factorplus=0.0;
     //predicted rate for order-1
     if (order>1){
-      cudaMemcpy(yerr, &(D[N*order]), N*sizeof(double), cudaMemcpyDeviceToDevice);
-      scale_err<<<(N+255)/256, 256>>>(yerr, yscale, yerr, N);
+      scale_err<<<(N+255)/256, 256>>>(&(D[N*order]), yscale, yerr, N);
       cublasDnrm2(handle, N, yerr, 1, &errnormminus);
-      errnormminus *= betaloc[order-1]/pow(N,0.5);
+      errnormminus *= (kappaloc[order-1]*gammabdfloc[order-1]+1.0/(order))/pow(N,0.5);
       factorminus=safety * pow(errnormminus, (-1.0 / (order)));
 
     }
     //predicted rate for order+1
     if (order<5){
-      cudaMemcpy(yerr, &(D[N*(order+2)]), N*sizeof(double), cudaMemcpyDeviceToDevice);
-      scale_err<<<(N+255)/256, 256>>>(yerr, yscale, yerr, N);
+      scale_err<<<(N+255)/256, 256>>>(&(D[N*(order+2)]), yscale, yerr, N);
       cublasDnrm2(handle, N, yerr, 1, &errnormplus);
-      errnormplus *= betaloc[order+1]/pow(N,0.5);
+      errnormplus *= (kappaloc[order+1]*gammabdfloc[order+1]+1.0/(order+2))/pow(N,0.5);
       factorplus=safety * pow(errnormplus, (-1.0 / (order + 2)));
     }  
+
+    // printf("errnormminus %e\n", errnormminus);
+    // printf("errnorm %e\n", errnorm);
+    // printf("errnormplus %e\n", errnormplus);
 
     int dorder=0;
     if (order>1 && factorminus > factor && factorminus > factorplus){
@@ -312,9 +313,22 @@ int bdf_step (double *t, double *h, void* pars){
 
     makeRU(RU, order, factor);
     cudaMemcpy(RU, RUloc, (order+1)*(order+1)*sizeof(double), cudaMemcpyHostToDevice);
+
+    // for (int i=0; i<order+1; i++){
+    //   cublasDnrm2(handle, N, &(D[N*i]), 1, &dnorm);
+    //   dnorm *= 1.0/pow(N,0.5);
+    //   printf("old Dnorm %i %e\n", i, dnorm);
+    // }
+    
     updateD<<<(N+255)/256, 256>>>(D, Dnew, N, order, RU);
     cudaMemcpy(D, Dnew, (order+1)*N*sizeof(double), cudaMemcpyDeviceToDevice);
-    
+
+    // for (int i=0; i<order+1; i++){
+    //   cublasDnrm2(handle, N, &(D[N*i]), 1, &dnorm);
+    //   dnorm *= 1.0/pow(N,0.5);
+    //   printf("new Dnorm %i %e\n", i, dnorm);
+    // }
+
     nconststeps=0;
     refactor=1;
   }
@@ -332,8 +346,13 @@ double* bdf_run(double *t, double *h, double t1, void *pars, void (*step_eval)(d
   (*dydt)(*t,y,f,pars);
   (*jac)((*t),y,&nnz,rows,cols,vals,pars);
 
-  makeA(&nnzA, -(*h)/(alphaloc[order]));
-
+  size_t bufferSize;
+  double factor=-(*h)/((1-kappaloc[order])*gammabdfloc[order]);
+  cusparseDcsrgeam2_bufferSizeExt(sphandle,N,N,&one,Idesc,N,Ivals,Irows,Icols,&factor,Jdesc,nnz,vals,rows,cols,Adesc,Avals,Arows,Acols,&bufferSize);
+  cudaMalloc(&buffer, bufferSize);
+  cusparseXcsrgeam2Nnz(sphandle,N,N,Idesc,N,Irows,Icols,Jdesc,nnz,rows,cols,Adesc,Arows,&nnzA,buffer);
+  
+  makeA(factor);
   cudssMatrixCreateCsr(&A, N, N, nnzA, Arows, NULL, Acols, Avals, CUDSS_R_32I, CUDSS_R_32I, CUDSS_R_64F, CUDSS_MTYPE_GENERAL, CUDSS_MVIEW_FULL, CUDSS_BASE_ZERO);
   cudssMatrixCreateDn(&dddss, N, 1, N, dd, CUDSS_R_64F, CUDSS_LAYOUT_COL_MAJOR);
   cudssMatrixCreateDn(&bdss, N, 1, N, b, CUDSS_R_64F, CUDSS_LAYOUT_COL_MAJOR);
@@ -342,10 +361,6 @@ double* bdf_run(double *t, double *h, double t1, void *pars, void (*step_eval)(d
   cudaMemcpy (&(D[0]), y, N*sizeof(double), cudaMemcpyDeviceToDevice);
   cudaMemcpy (&(D[N]), f, N*sizeof(double), cudaMemcpyDeviceToDevice);
   cublasDscal(handle, N, h, &(D[N]), 1);
-  cublasDscal(handle, N, &zero, dd, 1);
-
-  step0<<<(N+255)/256, 256>>>(ytemp, D, N, order);
-  rhs<<<(N+255)/256, 256>>>(b, f, d, D, N, order, *h, gammabdf, alpha);
 
   cudssExecute(dsshandle, CUDSS_PHASE_ANALYSIS, config, data, A, dddss, bdss);
   cudssExecute(dsshandle, CUDSS_PHASE_FACTORIZATION, config, data, A, dddss, bdss);
@@ -361,6 +376,11 @@ double* bdf_run(double *t, double *h, double t1, void *pars, void (*step_eval)(d
       (*step_eval)(*t,*h,y,pars);
     }
   }
+
+  cudaFree(buffer);
+  cudssMatrixDestroy(A);
+  cudssMatrixDestroy(dddss);
+  cudssMatrixDestroy(bdss);
   return y;
 }
 
@@ -394,10 +414,7 @@ double* bdf_init(int n, int nnzmax, double atol, double rtol, int fixedstep, dou
   cudaMalloc ((void**)&Dnew, 8*N*sizeof(double));
   
 
-  cudaMalloc ((void**)&kappa, 6*sizeof(double));
   cudaMalloc ((void**)&gammabdf, 6*sizeof(double));
-  cudaMalloc ((void**)&alpha, 6*sizeof(double));
-  cudaMalloc ((void**)&beta, 6*sizeof(double));
   cudaMalloc ((void**)&RU, 36*sizeof(double));
 
   cudaMalloc ((void**)&rows, (N+1)*sizeof(int));
@@ -427,9 +444,6 @@ double* bdf_init(int n, int nnzmax, double atol, double rtol, int fixedstep, dou
   cudaMemcpy (y, yloc, N*sizeof(double), cudaMemcpyHostToDevice);
   cudaMemcpy (D, yloc, N*sizeof(double), cudaMemcpyHostToDevice);
   cudaMemcpy (gammabdf, gammabdfloc, 6*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy (kappa, kappaloc, 6*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy (alpha, alphaloc, 6*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy (beta, betaloc, 6*sizeof(double), cudaMemcpyHostToDevice);
   cudssCreate(&dsshandle);
   cudssConfigCreate(&config);
   cudssDataCreate(dsshandle,&data);
